1f0332cb71497562d29311c73dacc090087502b9
Kaspits <azdanova97@gmail.com>
2023-06-30 11:55:30 +0200

    Update for HW6

--- six_lesson/HW6/LaptopFilter.java
+++ six_lesson/HW6/LaptopFilter.java
@@ -1,28 +1,28 @@
 package six_lesson.HW6;
 
-// import java.util.HashMap;
-// import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
 
-// public class LaptopFilter {
-// Map<String, String> filterSet;
+public class LaptopFilter {
 
-// public LaptopFilter() {
-// this.filterSet = new HashMap<>();
-// }
+    Map<String, String> filterSet;
 
-// public void add(String key, String value) {
-// if (value.length() > 0)
-// filterSet.put(key, value);
-// }
+    public LaptopFilter() {
+        this.filterSet = new HashMap<>();
+    }
 
-// public Laptop getFilterSample() {
-// int filterRam = Integer.parseInt(this.filterSet.getOrDefault("ram", "0"));
-// int filterHDDSize = Integer.parseInt(this.filterSet.getOrDefault("hddSize",
-// "0"));
-// String filterOs = this.filterSet.getOrDefault("os", "");
-// String filterColor = this.filterSet.getOrDefault("color", "");
-// String filterManufacturer = this.filterSet.getOrDefault("manufacturer", "");
-// return new Laptop(filterRam, filterHDDSize, filterOs, filterColor,
-// filterManufacturer);
-// }
-// }
+    public void add(String key, String value) {
+        if (value.length() > 0)
+            filterSet.put(key, value);
+    }
+
+    public Laptop getFilterSample() {
+        int filterRam = Integer.parseInt(this.filterSet.getOrDefault("ram", "0"));
+        int filterHDDSize = Integer.parseInt(this.filterSet.getOrDefault("hddSize", "0"));
+        String filterOs = this.filterSet.getOrDefault("os", "");
+        String filterColor = this.filterSet.getOrDefault("color", "");
+        String filterManufacturer = this.filterSet.getOrDefault("manufacturer", "");
+        return new Laptop(filterRam, filterHDDSize, filterOs, filterColor, filterManufacturer);
+    }
+
+}
